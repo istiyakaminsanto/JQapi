dd27bb8045f9dd8dd5d0a83f8ff93f1277720b37
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-09-03 13:06:55 +0600

    attr complete

--- js/attributes.js
+++ js/attributes.js
@@ -6,6 +6,43 @@ $(document).ready(function(){
         // if attributes exist then replace it by value
         $("#greatphoto").attr("alt", "my name is khan");
 
+        // passing value using object
+        $("#para1").attr({
+            class: "red",
+        });
+
+        // Set attribute and value using a function
+        $(".down-button").click(function(){
+            $("#greatphoto").attr("width", function(n, v){
+                return v - 50;
+            });
+        });
+
+        $(".up-button").click(function(){
+            $("#greatphoto").attr("width", function(n, v){
+                return parseInt(v) + 50;
+            });
+        });
+
+    
+
+        +function($inst){
+            let count = 1;
+            $inst.each(function(){
+                let $owner = $(this);
+                $owner.attr("id", "greatphoto" + count);
+                $( "span", $owner ).html( "(id='greatphoto" + count + "')" );
+                count++;
+
+            });
+        }($( "div [id]" ));
+        
+        $("div").text(function(){
+            $("img").attr("alt", function(){
+                return 
+            })
+        })
+
 });
 
 
