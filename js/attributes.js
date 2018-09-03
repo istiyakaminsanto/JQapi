09fc4007714701e2147079199574cdc321ce9e53
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-09-03 11:39:12 +0600

    attribute practise

---
+++ js/attributes.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    // attr method
+        // if attributes not exist then create new attributes
+        $("#greatphoto").attr("title", "Hello world");
+        // if attributes exist then replace it by value
+        $("#greatphoto").attr("alt", "my name is khan");
+
+});
+
+
